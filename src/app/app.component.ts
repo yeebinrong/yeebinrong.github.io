e5ddc13a8edca881b26eab1600c442e74172f81d
yeebinrong <yeebinrong@gmail.com>
2020-11-16 14:01:35 +0800

    'initial'

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'practice-angular';
+  cat = '/assets/images/cat1.png';
+  dog = '/assets/images/dog1.png';
+}
